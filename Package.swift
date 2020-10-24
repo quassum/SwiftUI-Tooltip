8e38a1eee9df4df9cd6cd8a1b5b9384f319e1d6e
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-10-24 18:00:55 +0300

    Added platform requirements

--- Package.swift
+++ Package.swift
@@ -5,24 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUI-Tooltip",
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6) 
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUI-Tooltip",
             targets: ["SwiftUI-Tooltip"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUI-Tooltip",
             dependencies: []),
-        .testTarget(
-            name: "SwiftUI-TooltipTests",
-            dependencies: ["SwiftUI-Tooltip"]),
+//        .testTarget(
+//            name: "SwiftUI-TooltipTests",
+//            dependencies: ["SwiftUI-Tooltip"]),
     ]
 )
