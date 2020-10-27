a64e58b7afaa875faff87810dfea338e515c7405
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-10-27 20:51:47 +0200

    Temporarily removed tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,7 @@
 import XCTest
 
-import SwiftUI_TooltipTests
-
-var tests = [XCTestCaseEntry]()
-tests += SwiftUI_TooltipTests.allTests()
-XCTMain(tests)
+//import SwiftUI_TooltipTests
+//
+//var tests = [XCTestCaseEntry]()
+//tests += SwiftUI_TooltipTests.allTests()
+//XCTMain(tests)
--- Tests/SwiftUI-TooltipTests/XCTestManifests.swift
+++ Tests/SwiftUI-TooltipTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
 import XCTest
 
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(SwiftUI_TooltipTests.allTests),
-    ]
-}
-#endif
+//#if !canImport(ObjectiveC)
+//public func allTests() -> [XCTestCaseEntry] {
+//    return [
+//        testCase(SwiftUI_TooltipTests.allTests),
+//    ]
+//}
+//#endif
